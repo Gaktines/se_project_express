c7f98d082949c9b55111113f005e8e01945b5b59
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-08-16 10:14:34 -0600

    jsupdates to users

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItems");
+const user = require('./users');
 
 router.use("/clothingItems", clothingItem);
+
+router.use("/users", user);
 
 router.use((req, res) => {
   res.status(500).send({ message: "No such Router" });
