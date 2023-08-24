df7d209a572a3199cb05c9524906f7af6e3271c0
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-08-24 13:35:22 -0600

    jsupdates to errors

--- utils/errors.js
+++ utils/errors.js
@@ -29,7 +29,7 @@ class CastError extends Error {
   constructor(message) {
     super(message);
     this.name = "CastError";
-    this.statusCode = 400;
+    this.statusCode = 404;
     this.message = "Cast Error";
   }
 }
